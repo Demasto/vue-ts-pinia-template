c52dd3df9af79b40994dfbc17a69b71296c977dd
Demasto <mr.d1men@yandex.ru>
2025-02-14 00:56:16 +0300

    added new classes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { Notifications } from "@kyvg/vue3-notification";
 </script>
 
 <template>
   <header>
-    <div>
-      <nav>
-        <RouterLink to="/home">Home</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-      </nav>
-    </div>
+    <nav class="flex-row align-center justify-center ga-4">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/login">Login</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
+  <Notifications classes="notification" :duration="8000"/>
+
 </template>
 
-<style scoped>
+<style lang="sass" scoped>
+@use '@/assets/scss/vars'
+
+header
+  background: vars.$color-card
+  height: vars.$header-height
+
+nav
+  height: 100%
 
 </style>
